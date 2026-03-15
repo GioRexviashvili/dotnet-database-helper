fe269ac6be9f0d4a390216d43277db79775eb846
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-03-15 16:15:27 +0400

    Add a test case in Program.cs

--- DatabaseHelper.App/Program.cs
+++ DatabaseHelper.App/Program.cs
@@ -8,12 +8,31 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        string connectionString = "";
+        string connectionString = "Server=localhost,1433;Database=Northwind;User Id=sa;Password=Giorgigamer707;TrustServerCertificate=True";
 
-        Database database = new Database(connectionString);
-        var connection = database.GetConnection();
-        var command = database.GetCommand("SELECT * FROM [Table]");
-        
+        using (var database = new Database(connectionString))
+        {
+            database.OpenConnection();
+            database.BeginTransaction();
+            try
+            {
+                int rowsAffected = database.ExecuteNonQuery(
+                    "insert into Categories (CategoryName, Description) values (@CategoryName, @Description)",
+                    new SqlParameter("@CategoryName", "category 001"),
+                    new SqlParameter("@Description", "description 001"));
+
+                Console.WriteLine($"Inserted {rowsAffected} rows");
+
+                database.CommitTransaction();
+                Console.WriteLine("Transaction committed");
+            }
+            catch (Exception ex)
+            {
+                database.RollbackTransaction();
+                Console.WriteLine($"Transaction rolled back: {ex.Message}");
+            }
+        } // Database.Dispose() gamoidzaxeba avtomaturad usingis blokis bolos
+
         // DatabaseHelper.Core.Database coreDatabase = new Core.Database(connectionString, new SqlServerDatabaseFactory());
         // coreDatabase.OpenConnection();
         //
